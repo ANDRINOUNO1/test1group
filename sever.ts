d96da302ad73f28f7ab7fbd11adeff87e163e2f3
unknown <you@example.com>
2025-03-07 11:07:39 +0800

    first commit

--- sever.ts
+++ sever.ts
@@ -1,1 +1,28 @@
-1231231
+import 'reflect-metadata';
+import dotenv from 'dotenv';
+dotenv.config();
+
+import express from 'express';
+import cors from 'cors';
+import { initializeDatabase } from './helpers/db';
+import userRouter from './users/user.controller';
+import { errorHandler } from './middleware/error.handler';
+
+const app = express();
+app.use(express.json());
+app.use(cors());
+app.use('/users', userRouter);
+app.use(errorHandler);
+
+const port = process.env.PORT || 4000;
+
+initializeDatabase()
+    .then(() => {
+        app.listen(port, () => {
+            console.log(`Server listening on port ${port}`);
+        });
+    })
+    .catch(error => {
+        console.error('Server startup error:', error);
+        process.exit(1);
+    });
